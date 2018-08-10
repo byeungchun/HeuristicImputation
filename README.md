0d1cba5fcab338bafd3590a0222666d1518cbb43
byeungchun.kwon <byeungchun.kwon@gmail.com>
2018-08-10 06:23:19 +0200

    Sphinx setting

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# hmli" 
+HMLI: Heuristic Machine-learning Imputation
+==============================================
+
+`HMIL`_is univeriate time series imputation method based on Heuristic search and machine learning.
+
+  
--- sphinx_make.bat
+++ sphinx_make.bat
@@ -7,8 +7,8 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=source
-set BUILDDIR=build
+set SOURCEDIR=sphinx_source
+set BUILDDIR=doc
 set SPHINXPROJ=HMLI
 
 if "%1" == "" goto help
