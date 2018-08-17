eb187097cf91e0de7257f42347a18b8f26e5c8de
Kwon, Byeungchun <ktwist@naver.com>
2018-08-17 18:06:44 +0200

    Update datahandler.py

--- hmli/datahandler.py
+++ hmli/datahandler.py
@@ -1,5 +1,5 @@
 import os
-from pandas import DataFrame, to_datetime, read_hdf
+from pandas import DataFrame, to_datetime, read_hdf, read_csv, to_datetime
 
 
 def load_bismacro_csv_into_dataframe(file_directory: str, csv_file: str) -> DataFrame:
@@ -51,3 +51,24 @@ def load_monthly_time_series_from_hdf5(hdf5_file, df_key='df') -> DataFrame:
     """
     df = read_hdf(hdf5_file, df_key)
     return df
+
+def convert_oecd_csv_to_hdf5(oecd_csv_file='MEIData-2018-08.csv', hdf5_file='mei.h5', df_key='df'):
+    """
+    Convert OECD MEI csv file and save it to hdf5 file 
+    OECD MEI: https://www.oecd-ilibrary.org/economics/data/main-economic-indicators_mei-data-en
+    """
+    df = read_csv(oecd_csv_file)
+    mm_cols = [df.columns[0]] #include series code
+    for i in df.columns[10:]: #take monthly series only
+        if i.find('M') > 0:
+            mm_cols.append(i)
+    df = df[mm_cols]
+    df.index = df.iloc[:,0]
+    df = df.iloc[:,-105:-5] #remove old data because there are many missing values
+    df = df.dropna()
+    df.columns = to_datetime(df.columns, format='%YM%m')
+    df = df.transpose()
+    df.to_hdf(hdf5_file,df_key)
+    return df
+              
+    
